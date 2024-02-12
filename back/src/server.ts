af56a2f1f86b154edf37dd911ddee2f1ee35ed0c
anton <antoniogallegobernal179@gmail.com>
2024-02-12 14:07:04 -0500

    Initial setup

---
+++ back/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+let app = express();
+
+let port = 3000;
+let host = "localhost";
+let protocol = "http";
+app.listen(port, host, () => {
+    console.log(`${protocol}://${host}:${port}`);
+});
