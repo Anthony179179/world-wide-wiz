2668e0dcbff8c08bb884905624dc29009b2a8f3a
Khang Duong <khangduong1519@gmail.com>
2024-02-20 23:35:47 -0500

    Added interactive map

--- front/src/components/MyMap.tsx
+++ front/src/components/MyMap.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import { useState } from "react";
+import { MapContainer, GeoJSON } from "react-leaflet";
+import mapData from "../data/countries.json";
+import "leaflet/dist/leaflet.css";
+import "./MyMap.css";
 
 function MyMap() {
+  let randomIndex: number = 0;
+
+  let countryStyle = {
+    fillColor: "red",
+    weight: 2,
+    color: "black",
+  };
+
+  let onEachCountry = (country, layer) => {
+    layer.on({
+      click: (event: any) => {
+        if (
+          event.target.feature.properties.ADMIN ===
+          mapData.features[randomIndex].properties.ADMIN
+        ) {
+          event.target.setStyle({
+            fillColor: "green",
+          });
+        } else {
+          event.target.setStyle({
+            fillColor: "red",
+          });
+        }
+        randomIndex = Math.floor(Math.random() * mapData.features.length);
+      },
+    });
+  };
+
   return (
     <>
       <div>
         <h1>Map</h1>
+        <div>{mapData.features[randomIndex].properties.ADMIN}</div>
+        <MapContainer></MapContainer>
+        <MapContainer
+          style={{ height: "80vh" }}
+          center={[20, 100]}
+          zoom={2}
+          attributionControl={false}
+        >
+          <GeoJSON
+            data={mapData.features}
+            style={countryStyle}
+            onEachFeature={onEachCountry}
+          />
+        </MapContainer>
       </div>
     </>
   );
