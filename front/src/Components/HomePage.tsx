02defbf899c56f7121605c554c8eb0976fb2b28c
Khang Duong <khangduong1519@gmail.com>
2024-03-16 22:41:16 -0400

    Added background for the homepage

--- front/src/Components/HomePage.tsx
+++ front/src/Components/HomePage.tsx
@@ -9,9 +9,52 @@ function HomePage() {
   return (
     <>
       <NavBar helloText="" loggedIn={false}></NavBar>
-      <h2>Welcome to the homepage!</h2>
-      <Link to={auth ? "/dashboard" : "/login"}> Log In </Link>
-      <Link to={auth ? "/dashboard" : "/signup"}> Sign Up </Link>
+      <div style={{ height: "100vh" }}>
+        <div
+          style={{
+            backgroundImage: "url(/earth.jpg)",
+            backgroundSize: "cover",
+            backgroundAttachment: "fixed",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
+          }}
+        >
+          <h1>Welcome!</h1>
+          <Link
+            style={{
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              padding: "10px 20px",
+              borderRadius: "5px",
+              textDecoration: "none",
+              margin: "10px",
+            }}
+            to={auth ? "/dashboard" : "/login"}
+          >
+            {" "}
+            Log In{" "}
+          </Link>
+          <Link
+            style={{
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              padding: "10px 20px",
+              borderRadius: "5px",
+              textDecoration: "none",
+              margin: "10px",
+            }}
+            to={auth ? "/dashboard" : "/signup"}
+          >
+            {" "}
+            Sign Up{" "}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
